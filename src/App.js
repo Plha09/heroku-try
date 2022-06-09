f6dd104761e11b870a7ad8448d478357cb730478
Plha <plhamo900@gmail.com>
2022-06-09 23:50:56 +0600

    added all files

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Routes, BrowserRouter} from "react-router-dom";
+import './App.css';
+import AddEdit from "./pages/AddEdit";
+import Dashboard from "./pages/Dashboard";
+import View from "./pages/View";
+import Layout from "./pages/layout";
+import About from "./pages/About";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter as  Route } from 'react-router-dom'
+
+
+export default function App() {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  return (
+    root.render(
+      <BrowserRouter>
+      <div>
+      <Header position="top-center" />
+      <ToastContainer position="top-center"/>
+      <Routes position="center"> 
+       <Route path="/" element={<Layout />}>
+          <Route exact index element={<Dashboard />} />
+          <Route path="/add" element={<AddEdit />} />
+          <Route path="/update/:id" element={<AddEdit />} />
+          <Route path="/view/:id" element={<View />} />
+          <Route path="/about" element={<About/>}/>
+        </Route>
+      </Routes>
+      </div>
+    </BrowserRouter>
+  
+
+    )
+    
+  );
+}
+
+
+ 
---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <p>This is most amazing platform to showcash your talents in Bhutan, 
+          <br/>
+         where you can uoload your favorite videos. This app is solely for entertainment.
+        </p>
+    </div>
+  )
+}
+
+export default About
